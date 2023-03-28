e2909cba6ff2998f386732c1a76e2646b9e2d63e
Vitor Santos Rodrigues <santos_vitao@hotmail.com>
2023-03-27 23:54:19 -0300

    create add like, comment delete/add

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,20 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
-export function Comment() {
+export function Comment({ content, onDeleteComment }) {
+
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleAddLike() {
+    setLikeCount(likeCount + 1);
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://scontent.fppb7-1.fna.fbcdn.net/v/t1.6435-9/95752418_2714779098644871_5081992427762352128_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEYRuiwo92t6mFRDfGZlOuOtPW9LRNQozK09b0tE1CjMgQF6rchH-Np-FW5T-DyK5RBalVa39rH8zgLVjJagYp0&_nc_ohc=clY_lWZ68_EAX9ujHrT&_nc_ht=scontent.fppb7-1.fna&oh=00_AfAIHcjqvUl9vf_LSKLa90q19TfHr6WxCfZs299M589N1A&oe=6442DAE6" />
@@ -15,16 +27,16 @@ export function Comment() {
               <time>Cerca de 1h atrás</time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={20} />
             </button>
           </header>
-          <p>Conteúdo comentário</p>
+          <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleAddLike}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,16 @@
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBr from 'date-fns/locale/pt-BR'
+import { useState } from 'react';
 
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
+
 import styles from './Post.module.css';
 
 export function Post({ author, content, publishedAt }) {
+
+  const [comments, setComments] = useState(['Que post massa, hein?!']);
+  const [newCommentText, setNewCommentText] = useState('');
 
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBr
@@ -14,6 +19,31 @@ export function Post({ author, content, publishedAt }) {
     locale: ptBr,
     addSuffix: true,
   });
+
+  function handleNewCommentTextChange() {
+    event.target.setCustomValidity('');
+    setNewCommentText(event.target.value);
+  }
+
+  function handleCreateComment() {
+    event.preventDefault();
+
+    setComments([...comments, newCommentText]);
+    setNewCommentText('');
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Esse campo é obrigatório!')
+  }
+
+  function handleDeleteComment(commentToDelete) {
+    const commentsWithoutDeletedComment = comments
+      .filter(comment => comment !== commentToDelete);
+
+    setComments(commentsWithoutDeletedComment);
+  }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -27,39 +57,48 @@ export function Post({ author, content, publishedAt }) {
             <span>{author.role}</span>
           </div>
         </div>
-        
+
         <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
           {publishedDateRelativeToNow}
         </time>
       </header>
-      
+
       <div className={styles.content}>
         {content.map(line => {
           if (line.type === 'paragraph') {
-            return <p>{line.content}</p>
-          } else if(line.type === 'link') {
-            return <p><a href='#'>{line.content}</a></p>
+            return <p key={line.content}>{line.content}</p>
+          } else if (line.type === 'link') {
+            return <p key={line.content}><a href='#'>{line.content}</a></p>
           }
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
+
         <textarea
+          name="comment"
           placeholder='Deixe um comentário'
+          value={newCommentText}
+          onChange={handleNewCommentTextChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type='submit'>Publicar</button>
+          <button type='submit' disabled={isNewCommentEmpty}>Publicar</button>
         </footer>
 
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment => (
+          <Comment
+            key={comment}
+            content={comment}
+            onDeleteComment={handleDeleteComment}
+          />
+        ))}
       </div>
     </article>
   )
